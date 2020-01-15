6f0388f7dc676d0380e4b005db539bba527abf5f
rhi-teetzel <rhiannateetzel@gmail.com>
2020-01-15 13:32:21 -0500

    initial commit

---
+++ DayThree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DayThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+          
+        {
+            int numberOfFriends = 2;
+            int costPerPerson = 13;
+            int totalBill = numberOfFriends * costPerPerson;
+
+            if (totalBill >= 50) 
+            {
+                Console.WriteLine("Congratulations! You receive 10% off!");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You receive 5% off!");               
+            }
+                Console.ReadKey();
+        }
+    }
+}
